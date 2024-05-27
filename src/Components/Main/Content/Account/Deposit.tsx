762765cd6768b794213d9a93a3c48c5488f19ad6
karavaichek <prohanter34@gmail.com>
2024-05-27 21:10:22 +0300

    add promocodes

--- src/Components/Main/Content/Account/Deposit.tsx
+++ src/Components/Main/Content/Account/Deposit.tsx
@@ -1,35 +1,75 @@
-import { Button, Input } from "antd"
+import { Button, Input, message } from "antd"
 import s from "../Account/Account.module.css"
-import { useDispatch } from "react-redux"
-import { depositThunk } from "../../../../store/authReducer"
-import { ChangeEvent, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { checkPromocodeThunk, depositThunk, setPromoCodCoefficient } from "../../../../store/authReducer"
+import { ChangeEvent, useEffect, useState } from "react"
+import { selectPromoState } from "../../../../store/selectors"
 
 
 const Deposit = () => {
 
+    const [messageApi, contextHolder] = message.useMessage();
     const [deposit, setDeposit] = useState("")
+    const [promocode, setPromocode] = useState("")
     const dispatch = useDispatch<any>()
+    const promoState = useSelector(selectPromoState)
+
+    const promoStrimg = promoState.promocodeCoefficient > 1 ? `(промокод +${promoState.promocodeCoefficient*100 - 100}%)` : ""
+    useEffect(() => {
+        if (promoState.promocodeCoefficient === -1) {
+            messageApi.open({
+                type: 'error',
+                content: 'Такого промокода не существует',
+            });
+            dispatch(setPromoCodCoefficient(1))
+        } else if (promoState.promocodeCoefficient > 1) {
+            messageApi.open({
+                type: 'success',
+                content: 'Промокод введён успешно',
+            });
+        }
+    }, [promoState.promocodeCoefficient])
+
     const depositOnClick = () => {
-        dispatch(depositThunk(Number(deposit)))
+        if (Number(deposit) > 0) {
+            dispatch(depositThunk(Number(deposit), promoState))
+        }
+    }
+
+    const checkPromoOnClick = () => {
+        if (promocode) {
+            dispatch(checkPromocodeThunk(promocode))
+        } else {
+            messageApi.open({
+                type: 'error',
+                content: 'Такого промокода не существует',
+            });
+        }
     }
 
     const depositOnChange = (e: ChangeEvent<HTMLInputElement>) => {
         setDeposit(e.currentTarget.value)
     }
 
+    const promocodeOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPromocode(e.currentTarget.value)
+    }
+
     return (
         <div>
+            {contextHolder}
             <div>Пополнить баланс:</div>
             <div className={s.deposit_container}>
-                <Input type="text" placeholder="ПРОМОКОД" />
-                <Button size="large" type="default">Проверить</Button>
+                <Input value={promocode} onChange={promocodeOnChange} type="text" placeholder="ПРОМОКОД" />
+                <Button onClick={checkPromoOnClick} size="large" type="default">Применить</Button>
                 {/* <Input type="text" placeholder="СУММА" />
                 <Button size="large" type="primary">Пополнить</Button> */}
             </div>
             <div className={s.deposit_container}>
-                <Input value={deposit} onChange={depositOnChange} type="text" placeholder="СУММА" />
+                <Input value={deposit} required={true} onChange={depositOnChange} type="number" placeholder="СУММА" />
                 <Button onClick={depositOnClick} size="large" type="primary">Пополнить</Button>
             </div>
+            <div className={s.depositResult}>Итого: {Math.floor(Number(deposit) * promoState.promocodeCoefficient)} {promoStrimg}</div>
         </div>
     )
 }
--- src/Components/Main/Content/Content.tsx
+++ src/Components/Main/Content/Content.tsx
@@ -3,12 +3,13 @@ import { Route, Routes } from "react-router-dom"
 import Account from "./Account/Account"
 import WithAuthRedirectHOC from "../../HOCs/WithAuthRedirectHOC"
 import Roulette from "./Roulette/Roulette"
+import TestRoll from "./Roulette/TestRoll"
 
 
 const Content = () => {
     return (
         <Routes>
-            <Route path="/games/roulette" element={<WithAuthRedirectHOC element={<Roulette />} />}/>
+            <Route path="/games/roulette" element={<WithAuthRedirectHOC element={<TestRoll />} />}/>
             <Route path="/account/me" element={<WithAuthRedirectHOC element={<Account />} />}/>
         </Routes>
     )
